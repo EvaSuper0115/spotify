f8d1f29343301fe1d6d7f9d251f754a0d28f4f4b
OnTai To <122745906+EvaSuper0115@users.noreply.github.com>
2023-08-07 11:00:28 +0100

    set up <Login/> component on client side, delete non-used files

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Container } from "react-bootstrap";
 export default function Login() {
   const AUTH_URL =
     "https://accounts.spotify.com/authorize?client_id=625a81e04da040f08e7974a7487b85b2&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
