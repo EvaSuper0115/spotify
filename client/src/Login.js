c9528657afdcb95e1f3bac3b6baf8662671e7111
OnTai To <122745906+EvaSuper0115@users.noreply.github.com>
2023-08-07 11:48:44 +0100

    set up server side , server.js and node

--- client/src/Login.js
+++ client/src/Login.js
@@ -2,8 +2,9 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import "./Login.css";
 export default function Login() {
-  const AUTH_URL =
-    "https://accounts.spotify.com/authorize?client_id=625a81e04da040f08e7974a7487b85b2&response_type=code&redirect_uri=http://localhost:8888/callback&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+  const client_id = "625a81e04da040f08e7974a7487b85b2";
+  const redirect_uri = "http://localhost:8888/callback";
+  const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
   return (
     <Container className="buttonContainer">
       <a
