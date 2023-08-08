d56b172154a214543b7f599d5b7873671e3338a0
OnTai To <122745906+EvaSuper0115@users.noreply.github.com>
2023-08-08 12:21:14 +0100

    solved localhost:3000 problem
    needed a / behind the 3000 in the code in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import Login from "./Login.js";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Dashboard from "./Dashboard.js";
 const code = new URLSearchParams(window.location.search).get("code");
 function App() {
-  return <Login />;
+  return code ? <Dashboard code={code} /> : <Login />;
 }
 
 export default App;
--- client/src/Login.js
+++ client/src/Login.js
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 import "./Login.css";
 export default function Login() {
   const client_id = "625a81e04da040f08e7974a7487b85b2";
-  const redirect_uri = "http://localhost:8888/callback";
+  const redirect_uri = "http://localhost:3000/";
   const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
   return (
     <Container className="buttonContainer">
