2d5c170c259a2d30c461429127f86fcfcc127aa3
OnTai To <122745906+EvaSuper0115@users.noreply.github.com>
2023-08-15 11:38:32 +0100

    found out the spotify IDs for the band for api calls

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -4,6 +4,15 @@ import useAuth from "./useAuth";
 import { Container, Form } from "react-bootstrap";
 import "./searchForm.css";
 export default function Dashboard({ code }) {
+  //https://open.spotify.com/artist/3Nrfpe0tUJi4K4DXYWgMUX
+  //Spotify ID of BTS is 3Nrfpe0tUJi4K4DXYWgMUX
+  //Spotify ID Jin is 5vV3bFXnN6D6N3Nj4xRvaV
+  //Spotify ID RM is 2auC28zjQyVTsiZKNgPRGs
+  //Spotify ID JHope is 0b1sIQumIAsNbqAoIClSpy
+  //Spotify ID Suga/Agust D is 5RmQ8k4l3HZ8JoPb4mNsML
+  //Spotify ID Jimin is 1oSPZhvZMIrWW5I41kPkkY
+  //Spotify ID V is 3JsHnjpbhX4SnySpvpa9DK
+  //Spotify ID Jungkook is 6HaGTQPmzraVmaVxvz6EUc
   const accessToken = useAuth(code);
   const [search, setSearch] = useState("top 10");
   return (
