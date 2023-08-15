4d652eed8f9a8422f58f234dd49d4c3937868bbc
OnTai To <122745906+EvaSuper0115@users.noreply.github.com>
2023-08-15 12:33:18 +0100

    Revert "installed spotify-web-api-node"
    This reverts commit 6f53a35632674501be8fbe197f8753745715d3c6.

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -3,15 +3,14 @@ import { useState } from "react";
 import useAuth from "./useAuth";
 import { Container, Form } from "react-bootstrap";
 import "./searchForm.css";
-import SpotifyWebApi from "spotify-web-api-node";
 export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
   const [search, setSearch] = useState("top 10");
   return (
     <Container className="searchFormContainer">
       <Form.Control
-        className="searchForm"
         autoFocus={true}
+        className="searchForm"
         type="search"
         placeholder="search BTS music by genre, mood or album etc."
         value={search}
