19e5a9f8fcb91cbbfe37e392a8912946715fec8f
OnTai To <122745906+EvaSuper0115@users.noreply.github.com>
2023-08-16 12:41:53 +0100

    now can get 200 respond

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import useAuth from "./useAuth";
 import { Container, Form } from "react-bootstrap";
 import "./searchForm.css";
+import SpotifyWebApi from "spotify-web-api-node";
+const spotifyApi = new SpotifyWebApi({
+  clientId: "625a81e04da040f08e7974a7487b85b2",
+});
 export default function Dashboard({ code }) {
   //https://open.spotify.com/artist/3Nrfpe0tUJi4K4DXYWgMUX
   //Spotify ID of BTS is 3Nrfpe0tUJi4K4DXYWgMUX
@@ -13,8 +17,31 @@ export default function Dashboard({ code }) {
   //Spotify ID Jimin is 1oSPZhvZMIrWW5I41kPkkY
   //Spotify ID V is 3JsHnjpbhX4SnySpvpa9DK
   //Spotify ID Jungkook is 6HaGTQPmzraVmaVxvz6EUc
+
   const accessToken = useAuth(code);
-  const [search, setSearch] = useState("top 10");
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    //only set access token if we have one, if we don't have, exit
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  useEffect(() => {
+    if (!search) return;
+    setSearchResult([]);
+    if (!accessToken) return;
+    spotifyApi
+      .searchTracks(search)
+      .then((res) => {
+        console.log(res.body);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [search, accessToken]);
+
   return (
     <Container className="searchFormContainer">
       <Form.Control
