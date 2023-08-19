3118bd31b0a2a66601918ddb90be01efdd0e8260
OnTai To <122745906+EvaSuper0115@users.noreply.github.com>
2023-08-19 11:46:41 +0100

    get artist section using res's images

--- client/src/ArtistInfo.js
+++ client/src/ArtistInfo.js
@@ -1,5 +1,23 @@
 import React from "react";
 import "./artistInfo.css";
 export default function ArtistInfo({ artistDetails }) {
-  return <div className="artistInfoSection">{artistDetails.name}</div>;
+  const backgroundImageStyle = {
+    backgroundImage: `url(${artistDetails.images[0].url})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    minHeight: "50vh",
+    minWidth: "50vh",
+  };
+  return (
+    <div className="artistInfoSection" style={backgroundImageStyle}>
+      <h1>{artistDetails.name}</h1>
+      <div className="artistStatus">
+        {" "}
+        {artistDetails.genres.map((genre, index) => (
+          <li key={index}>{genre}</li>
+        ))}
+        <li>Followers: {artistDetails.followers.total}</li>
+      </div>
+    </div>
+  );
 }
