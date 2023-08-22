97705f8a6209a6fecdadac5993b06f62f37f3904
OnTai To <122745906+EvaSuper0115@users.noreply.github.com>
2023-08-22 12:07:19 +0100

    sometimes it can't get anydata? maybe the accessToken expired?

--- client/src/BandInfo.js
+++ client/src/BandInfo.js
@@ -2,6 +2,17 @@ import React from "react";
 
 export default function BandInfo({ bandData }) {
   console.log(bandData);
-
-  return <div></div>;
+  const bandInfo = bandData.bandInfo;
+  const bandTopTracks = bandData.bandTopTracks;
+  const bandAlbums = bandData.bandAlbums;
+  return (
+    <div>
+      <h1>{bandInfo.name}</h1>
+      <h5>{bandInfo.followers.total} followers</h5>
+      <img src={bandInfo.images[0].url} alt="band-image" />
+      {bandInfo.genres.map((genre, index) => (
+        <div key={index}>{genre}</div>
+      ))}
+    </div>
+  );
 }
